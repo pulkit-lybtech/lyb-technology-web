0dc80c60371e361bfef1979438c3f1a42a6e76b8
pulkit-lybtechnlogy <pulkit.lybtechnology@gmail.com>
2021-07-19 20:08:13 +0530

    web service

---
+++ src/pages/Privacy.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Layout from '../components/layout'
+import Seo from '../components/seo'
+
+export default function Privacy() {
+	return (
+		<div>
+			<Layout>
+				<Seo title="Privacy Policy" />
+				<div className="projects-page">
+					<div className="page-header">
+						<div className="container">
+							<div className="row">
+
+								<div className="col-lg-8 mx-auto">
+									<h1 className="title">
+										Privacy Policy
+									</h1>
+									<p className="lead">
+										Some of our best work we completed in the past.
+									</p>
+								</div>
+
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</Layout>
+		</div>
+	)
+}
